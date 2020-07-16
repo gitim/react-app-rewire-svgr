bf45c6efefa2cefd0ad28aaff5038d402175e12e
Farzin Mirzaie <farzin.mirzaie@gmail.com>
2020-07-16 11:19:17 +0430

    Rewrite whole module
    Fixed "The "getBabelLoader" helper has been deprecated as of v2.0"
    
    issue #4

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
-const { getBabelLoader } = require(`react-app-rewired`)
+const path = require("path");
 
-function rewireSVGR(config, env, svgrLoaderOptions) {
-  const babelLoader = getBabelLoader(config.module.rules)
+const ruleChildren = (loader) =>
+  loader.use ||
+  loader.oneOf ||
+  (Array.isArray(loader.loader) && loader.loader) ||
+  [];
+
+const findIndexAndRules = (rulesSource, ruleMatcher) => {
+  let result;
+  const rules = Array.isArray(rulesSource)
+    ? rulesSource
+    : ruleChildren(rulesSource);
+  rules.some(
+    (rule, index) =>
+      (result = ruleMatcher(rule)
+        ? { index, rules }
+        : findIndexAndRules(ruleChildren(rule), ruleMatcher))
+  );
+  return result;
+};
+
+const createLoaderMatcher = (loader) => (rule) =>
+  rule.loader && rule.loader.indexOf(`${path.sep}${loader}${path.sep}`) !== -1;
+const fileLoaderMatcher = createLoaderMatcher("file-loader");
+
+const addBeforeRule = (rulesSource, ruleMatcher, value) => {
+  const { index, rules } = findIndexAndRules(rulesSource, ruleMatcher);
+  rules.splice(index, 0, value);
+};
+
+module.exports = function (config, env) {
   const svgReactLoader = {
     test: /\.svg$/,
-    use : [
-      {
-        loader : babelLoader.loader,
-        options: babelLoader.options,
-      },
-      { 
-        loader: require.resolve(`svgr/webpack`),
-        options: svgrLoaderOptions
-      },
-    ],
-  }
+    loader: require.resolve("@svgr/webpack"),
+  };
 
-  config.module.rules.unshift(svgReactLoader)
+  addBeforeRule(config.module.rules, fileLoaderMatcher, svgReactLoader);
 
-  return config
-}
-
-module.exports = rewireSVGR
+  return config;
+};
